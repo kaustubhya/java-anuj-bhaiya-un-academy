f20cf7a042509229ef2308e3e3f5ee843544f5d0
kaustubhya <kaustubhyaksd@gmail.com>
2022-10-04 13:58:53 +0530

    here

---
+++ java_oops2_L4.java
@@ -0,0 +1,20 @@
+public class java_oops2_L4 {
+    public static void main(String[] args) {
+        
+    }
+}
+    class MobilePhone{
+        int price;
+        int dimensions;
+        String os;
+
+    }
+
+    class Samsung extends MobilePhone {
+        
+    }
+
+    class Apple extends MobilePhone{
+        
+    }
+
