2a6f1510f992f17a1181f6860b031a852df86e16
kaustubhya <kaustubhyaksd@gmail.com>
2022-10-06 06:32:30 +0530

    done

--- java_oops2_L4.java
+++ java_oops2_L4.java
@@ -1,5 +1,12 @@
 public class java_oops2_L4 {
     public static void main(String[] args) {
+        Samsung s10 = new Samsung();
+        s10.playGames();
+
+        Apple a1 = new Apple();
+        a1.playGames();
+
+        s10.playGames(4);
         
     }
 }
@@ -8,10 +15,26 @@ public class java_oops2_L4 {
         int dimensions;
         String os;
 
+        void playGames(){
+            System.out.println("Mobiles phones hang a lot");
+        }
+
+        // Method Overloading
+
+        void playGames(int x){
+            for (int i = 0; i < x; i++){
+                System.out.println("Playing games is fun " + i);
+            }
+        }
+
     }
 
     class Samsung extends MobilePhone {
-        
+
+        // Method Overriding
+        void playGames(){
+            System.out.println("Samsung phones hang a lot");
+        }
     }
 
     class Apple extends MobilePhone{
